12251fc18ea8cc884281ef4c417c3befad370810
holyhost <2024212728@qq.com>
2024-09-12 18:02:42 +0800

    add about page

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import { Welcome } from "@/components/Welcome/Welcome";
+
+
+export default function AboutPage() {
+  return (
+    <>
+      <Welcome />
+      <p>this is about page</p>
+    </>
+  );
+}
