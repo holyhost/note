f5a5b2695f26c170d0fad9478fc06bf65238bbfa
holyhost <2024212728@qq.com>
2024-09-13 22:30:52 +0800

    add note interface

--- utils/model/Note.ts
+++ utils/model/Note.ts
@@ -1,14 +1,27 @@
-import { models, model, now, Schema } from "mongoose";
+import { models, model, Schema } from "mongoose";
 
 
-const NoteSchema = new Schema({
+export interface NoteModel {
+    _id?: string,
+    author: string,
+    title: string,
+    content: string,
+    ctime?: Date,
+    utime?: Date,
+    type: number,
+    view?: number,
+    tags?: string
+}
+
+const NoteSchema = new Schema<NoteModel>({
     author: String,
     title: String,
     content: String,
-    ctime: {type: Date, default: Date,now},
-    utime: {type: Date, default: Date,now},
+    ctime: {type: Date, default: Date.now},
+    utime: {type: Date, default: Date.now},
     type: {type: Number, default: 1},
-    view: Number
+    view: Number,
+    tags: String
 })
 
 const Note = models.Note ||  model('Note', NoteSchema)
