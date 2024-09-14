53b9589130f86858c805ee1da561667f22a27b69
holyhost <2024212728@qq.com>
2024-09-14 16:47:51 +0800

    update

--- components/Welcome/Welcome.tsx
+++ components/Welcome/Welcome.tsx
@@ -5,17 +5,17 @@ import { IconAlertCircle, IconHome, IconPaint, IconPencil, IconPlus } from '@tab
 export function Welcome() {
   return (
     <>
-      <Title className={classes.title} ta="center" mt={30}>
+      <Text className={classes.title} ta="center" mt={30}>
         Welcome to{' '}
         <Text inherit variant="gradient" component="span" gradient={{ from: 'pink', to: 'yellow' }}>
           Note
         </Text>
-      </Title>
+      </Text>
       <Group justify='center' mt={'lg'}>
-          <Button size='md' bg={'red'} c='white' leftSection={<IconHome/>} className='bdr-20' component='a' href='/'>主页</Button>
-          <Button size='md' bg={'teal'} c='white' leftSection={<IconPencil/>} className='bdr-20' component='a' href='/note'>笔记</Button>
-          <Button size='md' bg={'pink'} c='white' leftSection={<IconPlus/>} className='bdr-20' component='a' href='/note/add'>新增</Button>
-          <Button size='md' bg={'yellow'} c='white' leftSection={<IconAlertCircle/>} className='bdr-20' component='a' href='/about'>关于</Button>
+          <Button size='sm' bg={'red'} c='white' leftSection={<IconHome/>} className='bdr-20' component='a' href='/'>主页</Button>
+          <Button size='sm' bg={'teal'} c='white' leftSection={<IconPencil/>} className='bdr-20' component='a' href='/note'>笔记</Button>
+          <Button size='sm' bg={'pink'} c='white' leftSection={<IconPlus/>} className='bdr-20' component='a' href='/note/add'>新增</Button>
+          <Button size='sm' bg={'yellow'} c='white' leftSection={<IconAlertCircle/>} className='bdr-20' component='a' href='/about'>关于</Button>
       </Group>
       <Text c="teal" ta="center" size="lg" maw={580} mx="auto" mt="md">
         潭中鱼可百许头，皆若空游无所依。
