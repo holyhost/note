568bb90267b2debf9c72015d5745ee57168b2d96
holyhost <2024212728@qq.com>
2024-09-15 20:12:33 +0800

    add constant

--- utils/base.ts
+++ utils/base.ts
@@ -2,3 +2,22 @@
 export function getDomain() {
     return process.env.PUBLIC_APP_URL || "http://localhost:3000"
 }
+
+
+export interface BaseType {
+    label: string,
+    value: number
+}
+
+export const NOTE_TAGS: string[] = ['Error', 'React', 'Vue', 'Angular', 'Python', 'Nextjs']
+
+export const NOTE_TYPES: BaseType[] = [
+    {
+        label: '公开',
+        value: 1
+    },
+    {
+        label: '仅自己可见',
+        value: 11
+    },
+]
