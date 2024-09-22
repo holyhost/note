2478810650e386776d02e8d961477cc56691b0b2
holyhost <2024212728@qq.com>
2024-09-22 17:02:43 +0800

    fix loader showing

--- components/Note/NoteList.tsx
+++ components/Note/NoteList.tsx
@@ -19,27 +19,31 @@ const PAGE_COUNT = 6
 const NoteList = ({ notes, more = false }: Props) => {
     const [data, setData] = useState([...notes])
     const [end, setEnd] = useState(false)
+    const [loading, setLoading] = useState(false)
     const [page, sestPage] = useState(2)
     const [refView, inView] = useInView({ threshold: 0 })
 
     const loadMoreNotes = async ()=> {
+        setLoading(true)
         const apiNotes = await getNoteList(page)
         if(!apiNotes || apiNotes.length < PAGE_COUNT){
             setEnd(true)
         }
         sestPage(page + 1)
         setData([...data, ...apiNotes])
+        setLoading(false)
     }
     useEffect(()=> {
-        if(inView){
+        if(inView && !loading){
             loadMoreNotes()
         }
     }, [inView])
     return (
         <Container>
             {data.length && data.map((item: any, index: number) => (<NoteItem key={item._id} data={item} />))}
-            {more && notes.length && !end && <Center mt={'md'}>
-                <Loader ref={refView} color='teal' />
+            {more && notes.length && !end && <Center mt={'md'} mb={'xl'}>
+                <Text ref={refView}>{''}</Text>
+                {inView && <Loader color='teal' />}
             </Center>}
             {end && <Center mt={'md'} mb={'md'}>
                     <Text c={'gray'} size='sm'>-------已经滑到底啦-------</Text>
